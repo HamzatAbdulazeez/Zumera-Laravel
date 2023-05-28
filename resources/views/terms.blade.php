43936bbf42d662c1876c4f5f7bc6b752503a464b
Prof.T Tech <95409350+HamzatAbdulazeez@users.noreply.github.com>
2023-05-28 21:52:16 +0100

    updatye

--- resources/views/terms.blade.php
+++ resources/views/terms.blade.php
@@ -20,44 +20,44 @@
             <div class="col-lg-6 text-center">
                 <h3>TERMS & CONDITIONS</h3>
                 <p class="mb-4">
-                These terms and conditions ("Agreement") govern your use of the services provided
-by Zumera Property for all real estate transactions, including buying, selling,
-renting, leasing, or managing properties. By accessing or using  our services,
-you agree to be bound by these terms and conditions.
-If you do not agree with any part of this Agreement, you must refrain from using our services.
+                    These terms and conditions ("Agreement") govern your use of the services provided
+                    by Zumera Property for all real estate transactions, including buying, selling,
+                    renting, leasing, or managing properties. By accessing or using our services,
+                    you agree to be bound by these terms and conditions.
+                    If you do not agree with any part of this Agreement, you must refrain from using our services.
                 </p>
             </div>
             <div class="col-lg-10">
                 <div class="faq-box">
-                <details>
+                    <details>
                         <summary>Services</summary>
                         <div class="faq-content">
                             <p> a. The Company offers a range of real estate services, including property listings, property management, property valuations, and real estate advisory services.
-b. The Company may update or modify the services provided from time to time without prior notice.</p>
+                                b. The Company may update or modify the services provided from time to time without prior notice.</p>
                         </div>
                     </details>
                     <details>
                         <summary>User Responsibilities</summary>
                         <div class="faq-content">
                             <p>a. You are solely responsible for any content you provide, including property information, images, and any other data.
-b. You must ensure that all the information you provide is accurate, complete, and up to date.
+                                b. You must ensure that all the information you provide is accurate, complete, and up to date.
 
-c. You agree not to engage in any fraudulent, misleading, or illegal activities while using our services.
-d. You are responsible for maintaining the confidentiality of your account credentials and restricting access to your account.</p>
+                                c. You agree not to engage in any fraudulent, misleading, or illegal activities while using our services.
+                                d. You are responsible for maintaining the confidentiality of your account credentials and restricting access to your account.</p>
                         </div>
                     </details>
                     <details>
                         <summary>Property Listings</summary>
                         <div class="faq-content">
                             <p>a. Property listings provided by the Company are for informational purposes only and do not constitute an offer or guarantee of availability or suitability.
-b. The Company does not verify the accuracy or completeness of the property listings and is not responsible for any errors or omissions in the information provided.</p>
+                                b. The Company does not verify the accuracy or completeness of the property listings and is not responsible for any errors or omissions in the information provided.</p>
                         </div>
                     </details>
                     <details>
                         <summary>Fees and Payments</summary>
                         <div class="faq-content">
                             <p>a. The Company may charge fees for certain services, such as listing properties or property management. The applicable fees will be clearly communicated to you before using such services.
-b. You agree to pay all fees and charges associated with the services as per the payment terms provided by the Company.</p>
+                                b. You agree to pay all fees and charges associated with the services as per the payment terms provided by the Company.</p>
                         </div>
                     </details>
                     <details>
